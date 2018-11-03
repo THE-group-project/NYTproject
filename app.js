5884ee7d296cfa22589be56f9934c4cc37451d5e
ogesour <ogesour@gmail.com>
2018-11-03 13:10:10 -0700

    test

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello ferrin')
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "a8a6026d63e843659beeef6757c77320"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).then(function(){
+
+    
+})
+
