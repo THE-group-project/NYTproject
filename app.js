f86ca04a97e33b8e876c79683a0311dc787e518c
Finishoff <billvinhlam@gmail.com>
2018-11-03 13:06:41 -0700

    modified app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('hello ferrin')
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+var queryParam = { "api-key": "b9f91d369ff59547cd47b931d8cbc56b:0:74623931" };
+
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+
+});
